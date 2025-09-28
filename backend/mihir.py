55520a199179866ae9d1648a5d5f061426c5b378
Mihir1231 <mihir.rockshi@gmail.com>
2025-09-29 01:23:57 +0530

    Delete backend directory

--- backend/mihir.py
+++
@@ -1,9 +0,0 @@
-import chromadb
-
-DB_PATH = "D:/LDRP ITR/backend/chromadb_data/batch_2022-26"
-
-client = chromadb.PersistentClient(path=DB_PATH)
-
-print("Available collections:")
-for coll in client.list_collections():
-    print(" -", coll.name)
